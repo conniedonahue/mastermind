b242d12284d875a18f25aa97e0370fb575e91bb0
conniedonahue <connordonahue09@gmail.com>
2024-12-05 12:49:19 -0500

    Set up dotenv
    I decided to start building out the infrastrucure for production vs dev
    servers (even though I'm not sure I will be deploying this). I needed to
    create a secret key for my flask sessions, so I updated some of the
    environment logic in run.py. I created a makefile so that we can use
    `make dev` to run `FLASK_ENV=development FLASK_APP=run.py flask run `.

---
+++ makefile
@@ -0,0 +1,2 @@
+dev:
+	FLASK_ENV=development FLASK_APP=run.py flask run
--- run.py
+++ run.py
@@ -1,12 +1,23 @@
 from app import create_app
 from flask import render_template
+from dotenv import load_dotenv
+import os
+
+environment = os.getenv("FLASK_ENV", "development")
+env_file = f".env.{environment}"
+load_dotenv(env_file)
 
 app = create_app()
+app.secret_key = os.getenv('SECRET_KEY', 'fallback_key')
+
+if environment == "production" and not os.getenv("SECRET_KEY"):
+    raise RuntimeError("SECRET_KEY must be set in production!")
 
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    debug_mode = os.getenv("FLASK_DEBUG", "False") == "True"
+    app.run(debug=debug_mode)
 
